13d98e7c6c87d891ebbcc9a5e81a23e12d38fd7d
beedev-shubham <shubham@beedev.co.in>
2025-01-13 19:17:19 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
-const heading2 = React.createElement(
-  "h1",
-  {},
-  "I'm a H1 tag"
-);
-const child = React.createElement("div", {id: "child"}, [heading2, heading2]);
-const parent = React.createElement("div", {id: "parent"}, child);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const TitleComponent = () => <h1 classname="title">This is a Title</h1>;
+const HeadingComponent = () => {
+  return (<div className="container">
+    <TitleComponent />
+    <h1>Namaste React Function Component</h1>
+  </div>)
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
